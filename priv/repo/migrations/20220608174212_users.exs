5c6f72c52ed11fc0c9313e362271ca9dcb2a6b38
EugeneGorelenko <oldtoy480@gmail.com>
2022-06-08 21:03:00 +0300

    Create users migrations and users schema
    I'm sorry I'm crying, but how terrible it is :(

---
+++ priv/repo/migrations/20220608174212_users.exs
@@ -0,0 +1,16 @@
+defmodule ElixirProject.Repo.Migrations.Users do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :login, :string, null: false
+      add :password_hash, :string
+      add :email, :string, null: false
+
+      timestamps()
+    end
+
+    create unique_index :users, [:login]
+    create unique_index :users, [:email]
+  end
+end
